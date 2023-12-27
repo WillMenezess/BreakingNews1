a1ae5d1d349abaf7cf2c3dd227f32ce623f28484
William Menezes <will.mene40@hotmail.com>
2023-12-27 10:08:25 -0300

    #Added function findById ad

--- src/sevices/user.service.js
+++ src/sevices/user.service.js
@@ -6,8 +6,20 @@ const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id);
 
+const updateService = (
+    id,
+    name,
+    username,
+    password,
+    email,
+    avatar,
+    background
+) => User.findOneAndUpdate({ _id: id },
+    { name, username, password, email, avatar, background });
+
 module.exports = {
     createService,
     findAllService,
     findByIdService,
+    updateService,
 }
